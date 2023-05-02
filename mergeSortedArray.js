3b9e6b4c25fa5216a878d6824aff52de7554969d
maha <m4h4nk4l1@gmail.com>
2023-05-02 22:53:56 +0530

    improved solution

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -13,3 +13,20 @@ var merge = function(num1, m, num2, n) {
          }
 console.log(num1.sort((a,b)=>a-b))
 };
+
+
+
+//updated less time complexity and less execution time
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+        nums1.slice(0, m).concat(nums2).sort((a, b) => a - b).map((val, i) => nums1[i] = val);
+    };
+    
