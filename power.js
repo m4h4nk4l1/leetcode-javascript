e70e5369947c3546a7f954f9966fd3c9b46ca39c
maha <m4h4nk4l1@gmail.com>
2023-05-03 23:31:37 +0530

    power

---
+++ power.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+const binaryPow = (num, power) => {
+    if (num === 0) return 0;
+    if (power === 0) return 1;
+    if (power === 1) return num;
+    
+    const halfPower = Math.floor(power / 2);
+    const halfRes = binaryPow(num, halfPower);
+    const res = halfRes * halfRes;
+    
+    return power % 2 === 0 ? res : res * num;
+}
+
+const myPow = (x, n) => {
+    const res = binaryPow(x, Math.abs(n));
+    
+    return n < 0 ? 1/res : res;
+};
