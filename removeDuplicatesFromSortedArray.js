83c3739936e34520383b717b98d2678cdfe5653f
maha <m4h4nk4l1@gmail.com>
2023-05-10 23:33:30 +0530

    remove duplicates from sorted array

---
+++ removeDuplicatesFromSortedArray.js
@@ -0,0 +1,26 @@
+// Intuition
+// We can think of using two pointers ‘i’ and ‘j’, we move ‘j’ till we don’t get a number arr[j] which is different from arr[i]. As we got a unique number we will increase the i pointer and update its value by arr[j].
+
+// Approach
+// Take a variable i as 0;
+// Use a for loop by using a variable ‘j’ from 1 to length of the array.
+// If arr[j] != arr[i], increase ‘i’ and update arr[i] == arr[j].
+// After completion of the loop return i+1, i.e size of the array of unique elements.
+
+// Complexity
+// Time complexity:O(n)
+// Space complexity:O(1)
+// Code
+class Solution {
+    public int removeDuplicates(int[] arr) {
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+        
+    }
+}
