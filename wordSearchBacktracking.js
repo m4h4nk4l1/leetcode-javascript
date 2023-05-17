e3d7af069621ed7ef0ac7286618a4078eb40124e
maha <m4h4nk4l1@gmail.com>
2023-05-17 23:38:04 +0530

    Backtracking sum

---
+++ wordSearchBacktracking.js
@@ -0,0 +1,13 @@
+const exist = (board, word) => {
+    const traverse = (x, y, i) => {
+        if (i === word.length) return true
+        if (board[y]?.[x] !== word[i]) return false
+        board[y][x] = '#'
+        for (const [dx, dy] of [[-1, 0], [0, -1], [1, 0], [0, 1]])
+            if (traverse(x + dx, y + dy, i + 1))
+                return true
+        board[y][x] = word[i]
+        return false
+    }
+    return board.some((row, y) => row.some((_, x) => traverse(x, y, 0)))
+}
