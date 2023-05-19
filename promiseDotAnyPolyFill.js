cb72602b111cdf97a5acb71dd56e6555cf9b27c2
maha <m4h4nk4l1@gmail.com>
2023-05-19 23:58:35 +0530

    promise polyfills

---
+++ promiseDotAnyPolyFill.js
@@ -0,0 +1,19 @@
+const any = function(promisesArray) {
+    const promiseErrors = new Array(promisesArray.length);
+    let counter = 0;
+    //return a new promise
+    return new Promise((resolve, reject) => {
+    promisesArray.forEach((promise) => {
+    Promise.resolve(promise)
+    .then(resolve) // resolve, when any of the input promise resolves
+    .catch((error) => {
+        promiseErrors[counter] = error;
+        counter = counter + 1;
+        if (counter === promisesArray.length) {
+        // all promises rejected, reject outer promise
+        reject(promiseErrors);
+        }
+        });
+        });
+        });
+        };
